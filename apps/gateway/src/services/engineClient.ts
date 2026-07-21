45cb710e7a62820aad3d593f57854483dd42d94b
Nyries <cyprien.gaborieau@gmail.com>
2026-07-21 17:20:09 +0200

    feat: updating and adding sendCommand functions to match with the new format

--- apps/gateway/src/services/engineClient.ts
+++ apps/gateway/src/services/engineClient.ts
@@ -1,53 +1,69 @@
 import * as grpc from "@grpc/grpc-js";
-import * as protoLoader from "@grpc/proto-loader";
-import path from "path";
-import { fileURLToPath } from "url";
+import {
+  ApplyBrush,
+  CommandResponse,
+  PlaceEntity,
+  RemoveEntity,
+  SimulationServiceClient,
+  WorldStateResponse,
+} from "@app/shared-types";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const PROTO_PATH = path.join(
-  __dirname,
-  "../../../../packages/proto/simulation.proto",
-);
-
-const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
-  keepCase: true,
-  longs: String,
-  enums: String,
-  defaults: true,
-  oneofs: true,
-});
-
-const protoDescriptor = grpc.loadPackageDefinition(packageDefinition) as any;
-const simulationPackage = protoDescriptor.simulation;
 
 const ENGINE_URL = process.env.ENGINE_GRPC_URL || "localhost:50051";
-export const engineClient = new simulationPackage.SimulationService(
+export const engineClient = new SimulationServiceClient(
   ENGINE_URL,
   grpc.credentials.createInsecure(),
 );
 
-export function sendCommandToEngine(
-  actionType: string,
-  x: number,
-  y: number,
-  payload: object,
-) {
+export function sendApplyBrush(
+  parkId: string,
+  applyBrush: ApplyBrush,
+): Promise<CommandResponse> {
   return new Promise((resolve, reject) => {
-    engineClient.SendCommand(
+    engineClient.sendCommand(
       {
-        actionType: actionType,
-        x,
-        y,
-        payload: JSON.stringify(payload),
+        parkId: parkId,
+        command: { $case: "applyBrush", applyBrush },
       },
-      (error: grpc.ServiceError | null, response: any) => {
-        if (error) {
-          reject(error);
-        } else {
-          resolve(response);
-        }
+      (error: grpc.ServiceError | null, response: CommandResponse) => {
+        if (error) reject(error);
+        else resolve(response);
+      },
+    );
+  });
+}
+
+export function sendPlaceEntity(
+  parkId: string,
+  placeEntity: PlaceEntity,
+): Promise<CommandResponse> {
+  return new Promise((resolve, reject) => {
+    engineClient.sendCommand(
+      {
+        parkId: parkId,
+        command: { $case: "placeEntity", placeEntity },
+      },
+      (error: grpc.ServiceError | null, response: CommandResponse) => {
+        if (error) reject(error);
+        else resolve(response);
+      },
+    );
+  });
+}
+
+export function sendRemoveEntity(
+  parkId: string,
+  removeEntity: RemoveEntity,
+): Promise<CommandResponse> {
+  return new Promise((resolve, reject) => {
+    engineClient.sendCommand(
+      {
+        parkId: parkId,
+        command: { $case: "removeEntity", removeEntity },
+      },
+      (error: grpc.ServiceError | null, response: CommandResponse) => {
+        if (error) reject(error);
+        else resolve(response);
       },
     );
   });
@@ -55,12 +71,12 @@ export function sendCommandToEngine(
 
 export function subscribeToEngineStream(
   parkId: string,
-  onTick: (state: any) => void,
+  onTick: (state: WorldStateResponse) => void,
   onError: (err: any) => void,
 ) {
-  const call = engineClient.StreamState({ park_id: parkId });
+  const call = engineClient.streamState({ parkId: parkId });
 
-  call.on("data", (response: any) => {
+  call.on("data", (response: WorldStateResponse) => {
     onTick(response);
   });
 
