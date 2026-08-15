ab45f9d44ad7a61674ee0b8e4f196c998c5fd05e
Nyries <cyprien.gaborieau@gmail.com>
2026-08-15 18:04:28 +0200

    chores: adapting config files before shadcn

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -1,10 +1,21 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import path from "path"
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src")
+    }
+  },
+  server: {
+    watch: {
+      usePolling: true
+    }
+  },
   test: {
     environment: "jsdom",
     coverage: {
