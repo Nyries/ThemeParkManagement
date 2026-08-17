49f5e0ca30c6669cbac57f294a0d4039159d8921
Nyries <cyprien.gaborieau@gmail.com>
2026-08-17 08:03:13 +0200

    feat: engine versioning

--- apps/engine/build.rs
+++ apps/engine/build.rs
@@ -3,5 +3,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         &["../../packages/proto/simulation.proto"],
         &["../../packages/proto"],
     )?;
+
+    println!("cargo:rerun-if-changed=../../VERSION");
+    let version = std::fs::read_to_string("../../VERSION")?;
+    println!("cargo:rustc-env=APP_VERSION={}", version.trim());
+
     Ok(())
 }
